12500e74e1ea343557f3714accbfd02799d642b7
benls@iastate.edu <benls@iastate.edu>
2022-04-13 11:29:52 -0500

    Documented work from last night

--- Java Part 2/Assignment 3/Circle.java
+++ Java Part 2/Assignment 3/Circle.java
@@ -1,27 +1,64 @@
+/*
+ * Ben Schroeder
+ *
+ * A Circle object derived from the shape interface implementing its abstract methods
+ */
+
 public class Circle implements Shape{
+    /**
+     * I chose to use primitives and initialize them to 0 over using the double object. While using the double object
+     * would allow for me to set a null value and track whether a particular object has been given values, it would add
+     * unneeded complexity. This is because values of zero will not cause any errors with the current methods.
+     */
     private double radius;
 
+    /**
+     * Default Constructor
+     */
     public Circle() {
         radius = 0;
     }
 
+    /**
+     * Constructor for setting the radius upon creation
+     *
+     * @param radius the radius of the circle
+     */
     public Circle(double radius) {
         this.radius = radius;
     }
 
+    /**
+     * Getter for radius
+     *
+     * @return the radius of the circle
+     */
     public double getRadius() {
         return radius;
     }
 
+    /**
+     * Setter for radius
+     *
+     * @param radius the new radius of the circle
+     */
     public void setRadius(double radius) {
         this.radius = radius;
     }
 
+    /**
+     * Calculates the area of the circle
+     *
+     * @return the area of the circle
+     */
     @Override
     public double calculateArea() {
         return Math.PI * Math.pow(radius, 2);
     }
 
+    /**
+     * Prints a description of the object to the console
+     */
     @Override
     public void display() {
         System.out.printf("Circle with a radius of %s", radius);
--- Java Part 2/Assignment 3/Shape.java
+++ Java Part 2/Assignment 3/Shape.java
@@ -1,4 +1,19 @@
+/*
+ * Ben Schroeder
+ *
+ * A generic shape interface with abstract methods for area and printing a description of the shape
+ */
+
 public interface Shape {
+    /**
+     * Calculates the area of the shape
+     *
+     * @return the area of the shape
+     */
     double calculateArea();
+
+    /**
+     * Prints a description of the object to the console
+     */
     void display();
 }
